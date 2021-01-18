62db28f922859a6a60fc6d0b6235971ece9ce00a
Nicolas Galy <ngaly@hotmail.fr>
2021-01-18 15:15:20 +0100

    database connection

--- src/formation/java/tp/Main.java
+++ src/formation/java/tp/Main.java
@@ -1,9 +1,47 @@
 package formation.java.tp;
 
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args)
     {
+        // SQL Server avec précision de l'instance et du port d'écoute
+        String url = "jdbc:sqlserver://localhost\\SQLEXPRESS;database=Librairie;integratedSecurity=true;authenticationScheme=nativeAuthentication;";
+        // SQL Server avec instance et port d'écoute par défaut
+        //String url = "jdbc:sqlserver://localhost;databaseName=structures";
+//        String user = "user";
+//        String mdp = "";
+
+
+        Connection connexion = null;
+        Statement state = null;
+        PreparedStatement ps =  null;
+        ResultSet result = null;
+
+        try {
+            connexion = DriverManager.getConnection(url);
+            state = connexion.createStatement();
+            result = state.executeQuery("select * from Editors");
+            while(result.next()){
+                String Nom = result.getString("Name");
+                System.out.printf("Nom : " + Nom);
+            }
+            result.close();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (result != null)
+                    result.close();
+                if (state != null)
+                    state.close();
+                if (connexion != null)
+                    connexion.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
