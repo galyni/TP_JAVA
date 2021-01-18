6e85558145f001df7f3ba56a1146a401f6c83130
Flynt-hub <cerna.david.38@gmail.com>
2021-01-18 21:54:10 +0100

    [ADDED] Magazine class implemented

--- src/formation/java/tp/model/Magazine.java
+++ src/formation/java/tp/model/Magazine.java
@@ -5,27 +5,42 @@ import formation.java.tp.interfaces.IDisplayable;
 import formation.java.tp.utils.eMagazineType;
 import formation.java.tp.utils.ePublishmentFrequency;
 
+import java.util.Date;
+
 public class Magazine extends ACollectible implements IDisplayable
 {
-    public int                   mNumberOfPage;
-    public String                mAuthor;
-    public eMagazineType         mMagType;
-    public ePublishmentFrequency mFrequency;
+    private int                   mNumberOfPages;
+    private String                mAuthor;
+    private eMagazineType         mMagType;
+    private ePublishmentFrequency mFrequency;
 
     public Magazine (){}
-    public Magazine ( int pNumberOfPage, String pAuthor, eMagazineType pMagazineType, ePublishmentFrequency pFrequency )
+    public Magazine ( String pTitle, Editor pEditor, Date pPublishDate, int pNumberOfPages, String pAuthor, eMagazineType pMagazineType, ePublishmentFrequency pFrequency )
     {
-        mNumberOfPage = pNumberOfPage ;
-        mAuthor       = pAuthor ;
-        mMagType      = pMagazineType ;
-        mFrequency    = pFrequency ;
+        this.mPublishDate   = pPublishDate ;
+        this.mBorrowed      = false ;
+        this.mBorrowable    = false ;
+        this.mEditor        = pEditor ;
+        this.mTitle         = pTitle ;
+        this.mNumberOfPages = pNumberOfPages ;
+        this.mAuthor        = pAuthor ;
+        this.mMagType       = pMagazineType ;
+        this.mFrequency     = pFrequency ;
     }
+
+    public Date getPublishDate()           {return this.mPublishDate ;}
+    public boolean isBorrowed()            {return this.mBorrowed ;}
+    public void borrowBook()               {this.mBorrowed = true ;}
+    public Editor getEditor()              {return this.mEditor ;}
+    public String getTitle()               {return this.mTitle ;}
+    public int getNumberOfPages()          {return this.mNumberOfPages ;}
+    public String getAuthor()              {return this.mAuthor ;}
+    public eMagazineType getMagazineType() {return this.mMagType ;}
 
     @Override
     public String Stringify()
     {
-        String lToReturn = ( super.Stringify() +
-                             "cette article est un magazine " + this.mFrequency +" sur " + this.mMagType.name() + " de " + this.mNumberOfPage + " pages, " + " écrit par " + this.mAuthor) ;
-        return lToReturn;
+        return ( super.Stringify() +
+                 "cette article est un magazine " + this.mFrequency +" sur " + this.mMagType.name() + " de " + this.mNumberOfPages + " pages, " + " écrit par " + this.mAuthor) ;
     }
 }
