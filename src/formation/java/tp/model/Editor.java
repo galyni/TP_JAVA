be61af76598a394150f94218f59c126a35c61b2b
Flynt-hub <cerna.david.38@gmail.com>
2021-01-18 22:11:52 +0100

    [ADDED] Editor class implemented

--- src/formation/java/tp/model/Editor.java
+++ src/formation/java/tp/model/Editor.java
@@ -1,10 +1,42 @@
 package formation.java.tp.model;
 
-public class Editor
+import formation.java.tp.interfaces.IDisplayable;
+
+public class Editor implements IDisplayable
 {
-    public String mStreet;
-    public String mCity;
-    public String mCountry;
-    public String mName;
-    public String mSiret;
+    private String mStreet;
+    private String mCity;
+    private String mCountry;
+    private String mName;
+    private String mSiret;
+
+    public Editor(){}
+    public Editor( String pStreet, String pCity, String pCountry, String pName, String pSiret)
+    {
+        this.mStreet  = pStreet ;
+        this.mCity    = pCity ;
+        this.mCountry = pCountry ;
+        this.mName    = pName ;
+        this. mSiret  = pSiret ;
+    }
+
+    public String getEditorName()    {return this.mName ;}
+    public String getEditorCity()    {return this.mCity ;}
+    public String getEditorCountry() {return this.mCountry ;}
+    public String getEditorSiret()   {return this.mSiret ;}
+    public String getSiegeStreet()   {return this.mStreet ;}
+
+    public void setEditorCity(String pCity)       {this.mCity = pCity ;}
+    public void setEditorCountry(String pCountry) {this.mCountry = pCountry ;}
+    public void setSiegeStreet(String pStreet)    {this.mStreet = pStreet ;}
+
+    @Override
+    public String Stringify()
+    {
+        return ( this.mName + " : " +
+                 "\n\tVille : " + this.mCity +
+                 "\n\tRue : " + this.mStreet +
+                 "\n\tPays : " + this.mCountry +
+                 "\n\tSiret : " + this.mSiret) ;
+    }
 }
