dc2a88f612fec7dca60a3b45b9545322b3b7cd88
Flynt-hub <cerna.david.38@gmail.com>
2021-01-21 10:23:34 +0100

    [ADDED] DataWriter class, to write into file

---
+++ src/formation/java/tp/fileClasses/DataWriter.java
@@ -0,0 +1,47 @@
+package formation.java.tp.fileClasses;
+
+import formation.java.tp.abstracts.AFileWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DataWriter extends AFileWriter
+{
+    protected FileWriter mWriter ;
+
+    public DataWriter(){}
+    public DataWriter(String pFilePath)
+    {
+        this.mFilePath = pFilePath ;
+    }
+
+    public void SetFilePath( String pFilePath ){this.mFilePath = pFilePath ;}
+    public String GetFilePath()                {return this.mFilePath ;}
+
+    public void WriteFile(String pData, boolean pAppend)
+    {
+        try
+        {
+            this.mWriter = new FileWriter( this.mFilePath, pAppend ) ;
+        }
+        catch (IOException e)
+        {
+            System.out.println("Unable to open file.." + e.getMessage());
+            e.printStackTrace();
+        }
+        try
+        {
+            if(pAppend)
+            {
+                pData += "\n" ;
+            }
+            this.mWriter.write( pData ) ;
+            this.mWriter.close() ;
+        }
+        catch (IOException e)
+        {
+            System.out.println("Unable to write into file.." + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
